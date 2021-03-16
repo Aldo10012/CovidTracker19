0b4af567b9de066cf7d3ab1716f455556083844b
Alberto <aldo10012@gmail.com>
2021-03-16 15:03:19 -0700

    first commit

---
+++ CovidTracker19/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CovidTracker19
+//
+//  Created by Alberto Dominguez on 3/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CovidTracker19/CovidTracker19App.swift
@@ -0,0 +1,17 @@
+//
+//  CovidTracker19App.swift
+//  CovidTracker19
+//
+//  Created by Alberto Dominguez on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct CovidTracker19App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
