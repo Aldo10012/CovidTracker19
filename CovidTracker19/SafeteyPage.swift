742ae4f9fea60a4cc92205c88556b92475083aa5
Alberto <aldo10012@gmail.com>
2021-03-22 14:32:59 -0700

    [added] prevention UI to SafetyPage

--- CovidTracker19/SafeteyPage.swift
+++ CovidTracker19/SafeteyPage.swift
@@ -15,11 +15,17 @@ struct SafeteyPage: View {
         Symptom(name: "Fever"),
         Symptom(name: "Sore Throat")
     ]
+    var preventionList: [Prevention] = [
+        Prevention(img: "Wear A Mask", method: "Wear s mask", info: "some information will go here about how to preventthe spread and how it works"),
+        Prevention(img: "Wash Your Hands", method: "Wash your hands", info: "some information will go here about how to preventthe spread and how it works"),
+        Prevention(img: "Hand Sanitizer", method: "Use hand sanitizer", info: "some information will go here about how to preventthe spread and how it works"),
+        Prevention(img: "Social Distance", method: "Social distancing", info: "some information will go here about how to preventthe spread and how it works")
+    ]
     
     var body: some View {
         ZStack {
             RedBackground()
-                
+            
             VStack {
                 HStack(spacing: 20) {
                     Image("doctor-woman")
@@ -31,29 +37,37 @@ struct SafeteyPage: View {
                         .foregroundColor(.white)
                 }.padding(.horizontal, 30)
                 
-                Header(text: "Symptoms")
-                    .padding(.top, 50)
-                    .padding(.leading, 30)
-                    .padding(.bottom, 30)
                 
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 20){
-                        ForEach(symptomList) { symptom in
-                            SymptomCardView(symptom: symptom.name, img: symptom.name)
-                        }
-                    }.padding(.horizontal, 40)
-                    .padding(.bottom, 50)
+                ScrollView{
+                    Header(text: "Symptoms")
+                        .padding(.top, 50)
+                        .padding(.leading, 30)
+                        .padding(.bottom, 30)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack(spacing: 20){
+                            ForEach(symptomList) { symptom in
+                                SymptomCardView(symptom: symptom.name, img: symptom.name)
+                            }
+                        }.padding(.horizontal, 40)
+                        .padding(.bottom, 50)
+                    }
+                    
+                    Header(text: "Prevention")
+                        .padding(.leading, 30)
+                        .padding(.bottom, 25)
+                    
+                    ForEach(preventionList) { method in
+                        PreventionCardView(img: method.img, method: method.method, info: method.info)
+                    }
+                    
                 }
                 
-                Header(text: "Prevention")
-                    .padding(.leading, 30)
-                    .padding(.bottom, 25)
-                
-
-                    
                 Spacer()
             }
         }
+        
+        
     }
 }
 
@@ -98,6 +112,41 @@ struct Header: View {
             .frame(width: screen.width, alignment: .leading)
             .font(Font.system(size: 24, weight: .medium))
             .foregroundColor(Color(#colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1)))
-            
+        
+    }
+}
+
+struct Prevention: Identifiable{
+    let id = UUID()
+    var img: String
+    var method: String
+    var info: String
+}
+
+struct PreventionCardView: View {
+    @State var img: String
+    @State var method: String
+    @State var info: String
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(self.img)
+                .resizable()
+                .frame(width: screen.width*0.3, height: screen.width*0.3)
+            VStack(alignment: .leading, spacing: 10){
+                Text(self.method)
+                    .font(.title2)
+                    .fontWeight(.medium)
+                    .foregroundColor(.red)
+                Text(self.info)
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                
+            }
+        }.padding(10)
+        .frame(width: screen.width-30)
+        .background(Color.white)
+        .cornerRadius(15)
+        .shadow(color: Color(.black).opacity(0.15), radius: 8, x: 4, y: 8)
     }
 }
